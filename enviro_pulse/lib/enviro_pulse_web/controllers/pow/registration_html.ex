685cc952af399ff0ce0fac21d16c8421b0780e62
Karlos Daniel Pires da Silva <karlosdanielsilva7654@gmail.com>
2024-01-04 15:27:01 -0300

    feat: Add buttons to sign in with Google account
    Add buttons to sign in with Google account in the previously generated Pow authentication templates.

--- enviro_pulse/lib/enviro_pulse_web/controllers/pow/registration_html.ex
+++ enviro_pulse/lib/enviro_pulse_web/controllers/pow/registration_html.ex
@@ -1,5 +1,6 @@
 defmodule EnviroPulseWeb.Pow.RegistrationHTML do
   use EnviroPulseWeb, :html
+  import Phoenix.HTML.Tag
 
   embed_templates "registration_html/*"
 end
--- enviro_pulse/lib/enviro_pulse_web/controllers/pow/session_html.ex
+++ enviro_pulse/lib/enviro_pulse_web/controllers/pow/session_html.ex
@@ -1,5 +1,6 @@
 defmodule EnviroPulseWeb.Pow.SessionHTML do
   use EnviroPulseWeb, :html
+  import Phoenix.HTML.Tag
 
   embed_templates "session_html/*"
 end
